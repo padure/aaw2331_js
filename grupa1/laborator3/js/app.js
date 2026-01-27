06dbb99bb56152f39b14c454866ba4d409c0f983
padure <paduregheorghe7@gmail.com>
2026-01-27 12:38:01 +0200

    Laborator 3

---
+++ grupa1/laborator3/js/app.js
@@ -0,0 +1,54 @@
+//Sarcina 1
+// let nume = prompt("Scrie numele tau:");
+
+// if( isNaN(Number(nume)) ){
+//     alert(`Salut, ${nume}`);
+// }
+
+//Sarcina 2
+// let number = prompt("Scrie un numar:");
+
+// let rezultat = (Number(number) % 2 === 0 )
+//             ? "Numarul este par"
+//             : "Numarul este impar";
+// alert(rezultat);
+// console.log(rezultat);
+
+//Sarcina 3
+// let stare = confirm("Doresti sa continui?");
+// let mesaj = stare
+//     ? "Navigati cu placere"
+//     : "Pe curand!";
+
+// console.log(mesaj);
+// alert(mesaj);
+
+//Sarcina 4
+// let nr1 = 2026;
+// let str1 = "Marti";
+
+// let result = nr1 + " " + str1;
+// // let result = `${nr1} ${str1}`;
+// console.log(result);
+
+//Sarcina 5
+// let str = "";
+
+// if( !str.length ){
+//     console.log("Stringul este gol!");
+// }
+
+// Else If 
+// let color = "yellow";
+// if(color == "black"){
+//     console.log("Black");
+// } else if (color == "blue") {
+//     console.log("Blue");
+// } else if (color == "orange") {
+//     console.log("Orange");
+// } else if( color == "red"){
+//     console.log("Red");
+// } else{
+//     console.log("Nu avem asa culoare");
+// }
+
---
+++ lectia2/js/app.js
@@ -0,0 +1,31 @@
+let nr1 = 2026;
+let user = "John Doe";
+
+// if(typeof nr1 == "string"){
+//     console.log( nr1 );
+// }
+// console.log( typeof nr1 );
+
+// console.log(typeof (nr1 + user));
+
+// const orase = ["Cahul", "Cantemir", "Leova"];
+// const numere = Array(25, 54, 147);
+// console.log(orase);
+// console.log(numere);
+// JSON 
+// const oras = {
+//     "nume": "Cahul",
+//     "populatie": 35000,
+//     "pozitie": "Sud"
+// }
+
+// console.log(oras);
+
+// console.log( parseInt("25") );
+// console.log( parseFloat("25.2") );
+
+let nr2 = 2;
+
+if( 2 === "2" ){
+    console.log("True");
+}
