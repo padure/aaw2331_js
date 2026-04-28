ef282e0da51e3d75459b7b0578beca33cc1d14ce
padure <paduregheorghe7@gmail.com>
2026-04-28 11:00:14 +0300

    Pagina acasa

--- grupa1/laborator27/routes/index.js
+++ grupa1/laborator27/routes/index.js
@@ -1,9 +1,20 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const Task = require('../models/taskModel');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.redirect('/tasks');
+/**
+ * Ruta pentru Pagina de Acasă
+ * Extrage toate sarcinile pentru a popula statisticile și slider-ul
+ */
+router.get('/', async (req, res) => {
+    try {
+        const tasks = await Task.getAll();
+        // Trimitem sarcinile către view-ul views/index.ejs
+        res.render('index', { tasks });
+    } catch (error) {
+        console.error("Eroare la încărcarea paginii Home:", error);
+        res.status(500).send("Eroare server.");
+    }
 });
 
 module.exports = router;
